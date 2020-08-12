fb1802e9528a95fda4f2153f8985e7fc80412bc7
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-08-12 09:29:10 -0400

    Update publish.js

--- publish.js
+++ publish.js
@@ -1,10 +1,10 @@
-const ghPages = require('gh-pages');
+// const ghPages = require('gh-pages');
 
-ghPages.publish('dist', function(err) {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log('success');
-    ghPages.clean();
-});
+// ghPages.publish('dist', function(err) {
+//     if (err) {
+//         console.error(err);
+//         return;
+//     }
+//     console.log('success');
+//     ghPages.clean();
+// });
