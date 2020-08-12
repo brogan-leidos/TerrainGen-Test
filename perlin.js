47e491d8387f9d6099c0e790cacf74f0b556f958
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-08-12 10:34:17 -0400

    Update perlin.js

--- perlin.js
+++ perlin.js
@@ -1,19 +1,4 @@
 
-class Grad {
-  construtor(x, y, z) {
-    this.x = x; 
-    this.y = y; 
-    this.z = z;
-  };
-
-  dot2(x, y) {
-    return this.x*x + this.y*y;
-  };
-
-  dot3(x, y, z) {
-    return this.x*x + this.y*y + this.z*z;
-  };
-};
 
 export default class Noise {  
   constructor() {    
@@ -101,3 +86,20 @@ export default class Noise {
   };
 
 }
+
+
+class Grad {
+  construtor(x, y, z) {
+    this.x = x; 
+    this.y = y; 
+    this.z = z;
+  };
+
+  dot2(x, y) {
+    return this.x*x + this.y*y;
+  };
+
+  dot3(x, y, z) {
+    return this.x*x + this.y*y + this.z*z;
+  };
+};
