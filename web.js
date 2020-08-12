f42361222cad4facf18e462b8863d0809e4d774a
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-08-12 11:04:23 -0400

    Update web.js

--- web.js
+++ web.js
@@ -29,7 +29,9 @@ function generateNoise() {
       var cell = (x + y * canvas.width) * 4;
       var color = [];
       
-      if (value < 45) {
+      var shoreNoise = Math.floor(Math.random() * 5);
+      
+      if (value < 45-shoreNoise) {
         color = colorWater(cell);      
       }
       else if (value < 90) {
