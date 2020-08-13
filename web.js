cb69e06de0ad240909fa6ec10929e8eea7cc19b1
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-08-13 13:27:06 -0400

    Update web.js

--- web.js
+++ web.js
@@ -170,8 +170,8 @@ function colorNoise(avgNoise, data, fuzz, seaLevel, isHeightMap, canvas) {
 }
 
 function colorWater(value) {
-  var colorNoise = Math.floor(Math.random() * (50-value));
-  return [0, 0+colorNoise, 255-colorNoise, 255];
+  var colorNoise = Math.floor(Math.random() * (value));
+  return [0, 0+colorNoise/2, 200+colorNoise, 255];
 }
 
 function colorDeepWater(value) {
