46f350c4d48f1b9849d70936188db4e57baebd0a
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-08-14 08:41:00 -0400

    Update noiseGenWorker.js

--- noiseGenWorker.js
+++ noiseGenWorker.js
@@ -1,17 +1,17 @@
 import Noise from './perlin.js'
 
-onmessage = function(seedAdd, settings) {
- var noise = new Noise();
-  noise.seed(settings.seed + (seedAdd * .01));  
+// onmessage = function(seedAdd, settings) {
+//  var noise = new Noise();
+//   noise.seed(settings.seed + (seedAdd * .01));  
 
-  var noiseData = new Array();  
-  for (var x = 0; x < settings.canvas.width; x++) {
-    noiseData.push(new Array());
-    for (var y = 0; y < settings.canvas.height; y++) {
-      var value = Math.abs(noise.perlin2(x / settings.scale, y / settings.scale));
-      value *= 256;
-      noiseData[x].push(value);
-    }
-  } 
-  postMessage(noiseData);
-};
+//   var noiseData = new Array();  
+//   for (var x = 0; x < settings.canvas.width; x++) {
+//     noiseData.push(new Array());
+//     for (var y = 0; y < settings.canvas.height; y++) {
+//       var value = Math.abs(noise.perlin2(x / settings.scale, y / settings.scale));
+//       value *= 256;
+//       noiseData[x].push(value);
+//     }
+//   } 
+//   postMessage(noiseData);
+// };
